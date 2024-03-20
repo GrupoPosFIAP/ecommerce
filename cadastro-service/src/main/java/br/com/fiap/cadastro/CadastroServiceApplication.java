862c38f719804050340bf2cf57995a4a6f28e8d9
pradoo159 <emersonpradoo@hotmail.com>
2024-03-20 19:21:04 -0300

    Padronizando código

--- cadastro-service/src/main/java/br/com/fiap/cadastro/CadastroServiceApplication.java
+++ cadastro-service/src/main/java/br/com/fiap/cadastro/CadastroServiceApplication.java
@@ -1,15 +1,12 @@
 package br.com.fiap.cadastro;
 
-import br.com.fiap.rest.configuration.api.config.RestfulAutoConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EnableDiscoveryClient
-@ImportAutoConfiguration(classes = RestfulAutoConfiguration.class)
 public class CadastroServiceApplication {
 
 	public static void main(String[] args) {
